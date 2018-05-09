aa5fccecff43d8d7083e715807781112d3f688a3
tony90s <hxw1990@live.cn>
2018-05-09 09:25:02 +0800

    update meta viewport

--- hxw_blog/static/js/main.js
+++ hxw_blog/static/js/main.js
@@ -1,3 +1,19 @@
+window.onload = function () {
+    document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+            event.preventDefault()
+        }
+    });
+    var lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false)
+};
+
 function show_msg(selector, msg) {
     selector.removeClass("fade");
     selector.html(msg);
