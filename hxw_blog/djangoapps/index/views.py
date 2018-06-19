2d9545d39082850c235d1e0e170237edc00b2954
tony90s <hxw1990@live.cn>
2018-06-19 16:48:25 +0800

    redirect to microsite when visit blog in mobile device

--- hxw_blog/djangoapps/index/views.py
+++ hxw_blog/djangoapps/index/views.py
@@ -1,15 +1,21 @@
 import logging
 
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from article.models import Article
+from utils.context_processors import is_mobile
 
 logger = logging.getLogger('index.views')
 
 
 @ensure_csrf_cookie
 def index_views(request):
+    is_mobile_device = is_mobile(request)['is_mobile_device']
+    if is_mobile_device:
+        return HttpResponseRedirect(settings.MICROSITE)
+    
     template = 'index.html'
     context = {
         'article_type': {'value': 0, 'display_name': Article.get_type_name(0)}
